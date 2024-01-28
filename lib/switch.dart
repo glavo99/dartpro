aa6eac4e55f142919dd19d48b5fd49a6789b92a1
glavo99 <elgendy.yousef.1999@gmail.com>
2024-01-28 20:40:54 +0200

    added file

---
+++ lib/switch.dart
@@ -0,0 +1,19 @@
+void main() {
+  int num = 33;
+
+  switch (num) {
+    case 10:
+    case 33:
+      print("messi");
+    case 7:
+      print("cristiano ronaldo");
+    case 9:
+      print("mbabe");
+    case 1:
+      print("Alisson");
+    case 5:
+      print("sergio ramos");
+    default:
+      print("No such player with that number");
+  }
+}
