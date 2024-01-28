80ab6e7e470c79fe0eb562c15d1808d666351bbb
glavo99 <elgendy.yousef.1999@gmail.com>
2024-01-28 18:33:50 +0200

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 void main() {
-  int a = 5;
-  int b = 6;
-  int c = a + b;
-  print(c);
-  String? name;
-  print("My name is $name");
+  // int a = 5;
+  // int b = 6;
+  // int c = a + b;
+  // print(c);
+  // String? name;
+  // print("My name is $name");
+
+  int a = 10;
+  a ??= 20;
+  print(a);
 }
